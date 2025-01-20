a02c2871c7cfbf8b49a1a8e38311235531b14752
aleksesiy <lexa.svsk@gmail.com>
2025-01-20 20:37:47 +0700

    fix: task 1 dont use sync now

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,3 +1,9 @@
-const {readFile, readFileSync} = require('fs');
-const txt = readFileSync('./01-read-file/text.txt', 'utf-8');
-console.log(txt);
+const fs = require('fs')
+//const readFile = require('fs');
+fs.readFile('./01-read-file/text.txt', 'utf-8', (txt, err) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    console.log(txt);
+});
